b5d1ce31a74c2d9ddd4ef08f09906bb1afd4664e
jinwoojung <jwjung5038@gmail.com>
2020-03-08 01:52:04 +0900

    스토리북 실습완료

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
   stories: ['../src/**/*.stories.tsx'],
   addons: [
     '@storybook/preset-create-react-app',
-    '@storybook/addon-docs/react/preset'
+    '@storybook/addon-docs/react/preset',
+    '@storybook/addon-knobs/register'
   ]
 };
